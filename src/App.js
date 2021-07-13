0cd2e5cb6e593d764922fcf01bec0f176ac04fa4
mkdata95 <mkdata95@naver.com>
2021-07-13 18:57:30 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
+import React, { Component } from "react";
+import Customer from "./components/Customer";
 import "./App.css";
 
-import React, { Component } from "react";
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "19680826",
+    gender: "남자",
+    job: "직원",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "이미숙",
+    birthday: "1911116",
+    gender: "여자",
+    job: "학생",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "나잘난",
+    birthday: "20001826",
+    gender: "남자",
+    job: "개발자",
+  },
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <h2>LET's develpop management systems</h2>
+      <div>
+        {customers.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
       </div>
     );
   }
