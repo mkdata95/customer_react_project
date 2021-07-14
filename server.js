30453153abaa34b4081834aa595304e5c3393bad
mkdata95 <mkdata95@naver.com>
2021-07-14 17:55:25 +0900

    mysql 연결

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,33 +7,24 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync("./database.json", "utf8");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+connection.connect();
+
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/1",
-      name: "홍길동",
-      birthday: "19680826",
-      gender: "남자",
-      job: "직원",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2",
-      name: "이미숙",
-      birthday: "1911116",
-      gender: "여자",
-      job: "학생",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3",
-      name: "나잘난",
-      birthday: "20001826",
-      gender: "남자",
-      job: "개발자",
-    },
-  ]);
+  connection.query("SELECT * FROM customer", (err, rows, fields) => {
+    res.send(rows);
+    console.log(rows);
+  });
 });
 
 app.listen(port, () => console.log(`서버의 동작 포트는 ${port}`));
